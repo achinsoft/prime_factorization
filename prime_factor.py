f5d4b2e1bc400162b1f65ba82d7b10fcc071b898
AchinSoft <shaikh.raja@outlook.com>
2019-12-04 00:57:25 +0530

    test

--- prime_factor.py
+++ prime_factor.py
@@ -3,53 +3,52 @@ import time
 
 start_time = time.time()
 
-'''
 
-# starting thread 1
-t1.start()
-# starting thread 2
-t2.start()
-
-# wait until thread 1 is completely executed
-t1.join()
-# wait until thread 2 is completely executed
-t2.join()
-
-# creating thread
-t1 = threading.Thread(target=print_square, args=(10,))
-t2 = threading.Thread(target=print_cube, args=(10,))
-'''
-
-
-def hunt_prime(number_value_start, number_value_end):
-    for numbers in range(number_value_start, number_value_end):
+def hunt_prime(number_value_start_func, number_value_end_func):
+    for numbers in range(number_value_start_func, number_value_end_func):
         is_prime = True
         for number in range(2, (int(numbers / 2) + 1)):
+        #for number in range(2, numbers):
             mod_value = numbers % number
             if mod_value == 0:
                 is_prime = False
                 break
         if is_prime:
             global prime_number_count
-           # print(numbers)
+            #print(numbers)
             prime_number_count += 1
 
 
-number_value = 100000
-half_point_number = int(number_value / 2)
-#print(half_point_number)
+number_value = 1000000
+total_thread = 4
+segment_number = int(number_value / 4)
+# print(half_point_number)
 prime_number_count = 0
 
 # creating thread
 number_value_start = 2
-number_value_end = half_point_number
+number_value_end = segment_number
+print(f"t1 start {number_value_start}  end {number_value_end}")
 t1 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
-number_value_start = half_point_number
-number_value_end = number_value
+number_value_start = segment_number+1
+number_value_end = segment_number*2
+print(f"t2 start {number_value_start}  end {number_value_end}")
 t2 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
+number_value_start = segment_number*2+1
+number_value_end = segment_number*3
+print(f"t3 start {number_value_start}  end {number_value_end}")
+t3 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
+number_value_start = segment_number*3+1
+number_value_end = number_value
+print(f"t4 start {number_value_start}  end {number_value_end}")
+t4 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
 t1.start()
 t2.start()
+t3.start()
+t4.start()
 t1.join()
 t2.join()
+t3.join()
+t4.join()
 print(f"Total prime numbers between 2-{number_value} is {prime_number_count}")
 print("--- %s mins ---" % ((time.time() - start_time) / 60))
