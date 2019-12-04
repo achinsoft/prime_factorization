961dc3cf3fd106728ce08a0bd37dc9951a10817c
Achinsoft <shaikh.raja@outlook.com>
2019-12-04 12:03:18 +0530

    test

--- prime_factor.py
+++ prime_factor.py
@@ -24,35 +24,45 @@ number_value_start = int(input("Enter the start value : "))
 
 start_position = number_value_start
 number_value = int(input("Enter the end value : "))
+total_number_check = number_value-start_position
+print(f"Total number will be checked : {total_number_check}")
 total_thread = 4
-segment_number = int(number_value / 4)
-# print(half_point_number)
+segment_number = int(total_number_check / 4)
+print(segment_number)
 prime_number_count = 0
 prime_number_list = ""
 # creating thread
-number_value_end = segment_number
+number_value_end = number_value_start + segment_number
 print(f"t1 start {number_value_start}  end {number_value_end}")
 t1 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
-number_value_start = segment_number + 1
-number_value_end = segment_number * 2
+number_value_start = number_value_end + 1
+number_value_end = number_value_end + segment_number
 print(f"t2 start {number_value_start}  end {number_value_end}")
 t2 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
-number_value_start = segment_number * 2 + 1
-number_value_end = segment_number * 3
+number_value_start = number_value_end + 1
+number_value_end = number_value_end + segment_number
 print(f"t3 start {number_value_start}  end {number_value_end}")
 t3 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
-number_value_start = segment_number * 3 + 1
+number_value_start = number_value_end + 1
 number_value_end = number_value
 print(f"t4 start {number_value_start}  end {number_value_end}")
 t4 = threading.Thread(target=hunt_prime, args=(number_value_start, number_value_end,))
 t1.start()
+print("Thread1 started")
 t2.start()
+print("Thread2 started")
 t3.start()
+print("Thread3 started")
 t4.start()
+print("Thread4 started")
 t1.join()
+print("Thread1 end")
 t2.join()
+print("Thread2 end")
 t3.join()
+print("Thread3 end")
 t4.join()
+print("Thread4 end")
 print(f"Total prime numbers between {start_position} -{number_value} is {prime_number_count}")
 print("--- %s mins ---" % ((time.time() - start_time) / 60))
 f = open("prime.txt", "w")
