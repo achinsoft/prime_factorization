1fea73959926204c8f09f88d0c21f16ce858e3a4
Achinsoft <shaikh.raja@outlook.com>
2019-12-06 21:11:06 +0530

    test

--- test.py
+++ test.py
@@ -1,2 +1,36 @@
-for n in range(3,10,2):
-    print(n)
+from multiprocessing import Process, Queue
+import time
+
+
+def hunt_prime(number_to_check):
+    is_prime = True
+    for number in range(2, (int(number_to_check / 2) + 1)):
+        # for number in range(2, numbers):
+        mod_value = number_to_check % number
+        if mod_value == 0:
+            is_prime = False
+            break
+    if is_prime:
+        f1 = open("prime.txt", "a")
+        f1.write(str(number_to_check) + "\n")
+        f1.close()
+
+
+if __name__ == '__main__':
+    f = open("prime.txt", "w")
+    f.close()
+    number_value_start = int(input("Enter the start value : "))
+    if number_value_start < 2:
+        print("Invalid data..Auto Set to Default value")
+        number_value_start = 2
+    start_position = number_value_start
+    number_value = int(input("Enter the end value : "))
+    if number_value < number_value_start:
+        print("Invalid data..Auto Set to Default value")
+        number_value = number_value_start + 1
+    start_time = time.time()
+    total_number_check = number_value - start_position
+    print(f"Total number will be checked : {total_number_check}")
+    total_thread = 4
+    print(f"Total thread {total_thread}")
+    t1 = Process(target=hunt_prime, args=(number_value_start,))
